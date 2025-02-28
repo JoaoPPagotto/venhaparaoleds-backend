f8204781d659dc674984316b5de444354bfa1c23
JoaoPPagotto <pagotto.costa@gmail.com>
2025-02-28 14:17:51 -0300

    gitignore

--- venhaparaoleds-backend-C#/obj/Debug/net8.0/venhaparaoleds-backend-C#.AssemblyInfo.cs
+++ venhaparaoleds-backend-C#/obj/Debug/net8.0/venhaparaoleds-backend-C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("venhaparaoleds-backend-C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c4cf303511f3120e0ee6c7aff76ee8792bd7a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5443da3b5e9c75ed8f217f8f847b4072dd09905c")]
 [assembly: System.Reflection.AssemblyProductAttribute("venhaparaoleds-backend-C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("venhaparaoleds-backend-C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
